5f401b73e94e9e030848b789e05a0d4e0a07c935
Balinhh09 <vulinh30061997@gmail.com>
2025-11-19 11:06:33 +0700

    Add files via upload

---
+++ src/EX02.java
@@ -0,0 +1,6 @@
+public class EX02 {
+    public static void main(String[] args) {
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại Rikkei Academy. Quê ở %s",
+                "Linh", 28, "Nha Trang" );
+    }
+}
